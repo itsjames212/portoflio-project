76c977d277c3a03a371ac16f598566e64d0481ae
Moses <mosesjuniorb@gmail.com>
2022-12-05 19:19:12 -0500

    hugeee changes and finished with nav bar

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-function darkModeToggle(){
-    document.querySelector("body").classList.toggle('dark--mode')
+function toggleSettings() {
+  document.querySelector(".settings--modal").classList.toggle("modal--open");
+  document.querySelector('main').style.opacity = 0;
+}
+function removeOpacity(){
+    document.querySelector('main').style.opacity = 1;
+    document.querySelector('.settings--modal').classList.remove('modal--open');
 }
